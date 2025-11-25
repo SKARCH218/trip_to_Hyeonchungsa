240053aef817b917763c97919cbe7dcda1e80753
beuljag <54463802L@gmail.com>
2025-11-25 20:07:14 +0900

    [+] create background.kt

---
+++ app/src/main/java/com/example/trip_to_hyeonchungsa/tthLib/background.kt
@@ -0,0 +1,28 @@
+package com.example.trip_to_hyeonchungsa.tthLib
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+
+@Composable
+fun SetBackground(imageName: String, modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    val context = LocalContext.current
+    val resourceId = context.resources.getIdentifier(imageName, "drawable", context.packageName)
+
+    Box(modifier = modifier.fillMaxSize()) {
+        if (resourceId != 0) {
+            Image(
+                painter = painterResource(id = resourceId),
+                contentDescription = "background image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+        content()
+    }
+}
