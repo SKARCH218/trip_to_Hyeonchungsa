0f1f026bed49bcd3ce8829b3ad0f1b79916be0b8
SKARCH <kimheuu0218@gmail.com>
2025-11-26 11:09:10 +0900

    [C] MainActivity.kt 코드를 tthLib의 문법에 맞게 수정 [-] 불필요한 ExampleInstrumentedTest.kt, ExampleUnitTest.kt 삭제

--- app/src/main/java/com/example/trip_to_hyeonchungsa/MainActivity.kt
+++ app/src/main/java/com/example/trip_to_hyeonchungsa/MainActivity.kt
@@ -8,22 +8,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.trip_to_hyeonchungsa.R // 패키지명에 맞게 R 클래스 import 필요
 
 import com.example.trip_to_hyeonchungsa.tthLib.Bubble
+import com.example.trip_to_hyeonchungsa.tthLib.SetBackground
 
 @Composable
 fun QuestDialogueScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        // 1. 배경 이미지
-        // R.drawable.img_example 부분을 실제 이미지 리소스로 변경해주세요.
-        // 이미지가 없으면 임시로 주석 처리하고 배경색을 넣어서 테스트하세요.
-
-        Image(
-            painter = painterResource(id = R.drawable.img_example),
-            contentDescription = "Background",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize()
-        )
+    SetBackground(imageName = "img_example") {
 
         Bubble(
             name = "오누이",
